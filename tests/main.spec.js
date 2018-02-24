3af1fb8bb5d636d5ccc0434a12a3455ded367535
Luiz Felipe <lflimeira02@gmail.com>
2018-02-24 00:15:43 -0300

    Refactors code to ES6

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,60 +1,56 @@
-let expect = require('chai').expect
-let calc = require('../src/main.js')
+import { expect } from 'chai'
+import { sum, sub, mult, div } from '../src/main.js'
 
 describe('Calc', () => {
   // smoke tests
   describe('smoke tests', () => {
     
-    it('should exist the calc lib', () => {
-      expect(calc).to.exist
-    })
-    
     it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist 
+      expect(sum).to.exist 
     })
 
     it('should exist the method `sub`', () => {
-      expect(calc.sub).to.exist 
+      expect(sub).to.exist 
     })
     
     it('should exist the method `mult`', () => {
-      expect(calc.mult).to.exist 
+      expect(mult).to.exist 
     })
     
     it('should exist the method `div`', () => {
-      expect(calc.div).to.exist 
+      expect(div).to.exist 
     })
   })
 
   describe('Sum', () => {
     it('should return 4 when `sum(2,2)`', () => {
-      expect(calc.sum(2,2)).to.be.equal(4);
+      expect(sum(2,2)).to.be.equal(4);
     })
   })
   
   describe('Sub', () => {
     it('should return 4 when `sub(6,2)`', () => {
-      expect(calc.sum(2,2)).to.be.equal(4);
+      expect(sum(2,2)).to.be.equal(4);
     })
     
     it('should return -4 when `sub(6,10)`', () => {
-      expect(calc.sub(6,10)).to.be.equal(-4);
+      expect(sub(6,10)).to.be.equal(-4);
     })
   })
   
   describe('Mult', () => {
     it('should return 4 when `mult(2,2)`', () => {
-      expect(calc.mult(2,2)).to.be.equal(4);
+      expect(mult(2,2)).to.be.equal(4);
     })
   })
   
   describe('Div', () => {
     it('should return 2 when `div(4,2)`', () => {
-      expect(calc.div(4,2)).to.be.equal(2);
+      expect(div(4,2)).to.be.equal(2);
     })
     
     it('should return `não é possível divisão por zero` when divide by 0', () => {
-      expect(calc.div(4,0)).to.be.equal('não é possível divisão por zero');
+      expect(div(4,0)).to.be.equal('não é possível divisão por zero');
     })
   })
 })
