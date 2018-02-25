567de675276c48326418039491e3fc083b98eeb4
Luiz Felipe <lflimeira02@gmail.com>
2018-02-24 23:23:32 -0300

    Spotify Web API initial commit

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,35 +1,5 @@
 let expect = require('chai').expect;
 
 describe('Main', () => {
-  let arr
-
-  //Roda todas as vezes antes de cada bloco
-  beforeEach(() => {
-    arr = [1,2,3]
-  })
-  
-  it('should be an array', () => {
-    expect(arr).to.be.a('array')
-  })
-
-  it('should have a size of 4 when push another value to the array',() => {
-    arr.push(4)
-    expect(arr).to.have.lengthOf(4)
-  })
-  
-  it('should have a size of 2 when pop a value from the array',() => {
-    arr.pop()
-    expect(arr).to.have.lengthOf(2)
-  })
-
-  it('should return true when pop 3 from the array', () => {
-    expect(arr.pop() === 3).to.be.true
-  })
-
-  it('should remove the value 3 when use pop in the array',() => {
-    arr.pop()
-    expect(arr).to.not.contain(3)
-  })
-
 
 })
