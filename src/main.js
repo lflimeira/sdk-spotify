b6db1be58a1aad231dc790b0be3851b1cfad1dcd
Luiz Felipe <lflimeira02@gmail.com>
2018-02-24 23:53:42 -0300

    Creates main functions and smoke tests

---
+++ src/main.js
@@ -0,0 +1,5 @@
+export const search = () => {};
+export const searchArtists = () => {};
+export const searchAlbuns = () => {};
+export const searchTracks = () => {};
+export const searchPlaylists = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,35 @@
-let expect = require('chai').expect;
+import { expect } from 'chai'
+import { 
+  search, 
+  searchAlbuns,
+  searchArtists,
+  searchTracks,
+  searchPlaylists
+} from '../src/main'
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+
+  describe('smoke tests', () => {
+    
+    it('should exist the search method', () => {
+      expect(search).to.exist
+    })
+    
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist
+    })
+    
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist
+    })
+    
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist
+    })
+    
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist
+    })
+  })
 
 })
