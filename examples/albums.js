a19b62503f4c6455f38696be11f791b772564d19
Luiz Felipe <lflimeira02@gmail.com>
2018-03-15 01:34:00 -0300

    Refactor example files albums.js

--- examples/albums.js
+++ examples/albums.js
@@ -1,7 +1,12 @@
+/* to run: babel-node albums.js */
 global.fetch = require('node-fetch')
 
-import { searchAlbums } from '../src/main'
+import SpotifyWrapper from '../src/index'
 
-const albums = searchAlbums('Incubus')
+const spotify = new SpotifyWrapper({
+  token: 'BQAfwkvX1S9GQACQTMusUqG9RUg-egTC2Bj7Q3C3RnNqzH5oWJxL_xNR5hx9H4L5-2IMeKRYxFPOjPG1wD--sHHZZfvVywnrDzk_qhUno4JAgc8ULEPSsGwzc5WkmxHI6aNde2cAidQZBzXp'
+})
+
+const albums = spotify.search.searchAlbums('Incubus')
 
 albums.then(data => data.albums.items.map(item => console.log(item.name)))
