a313c49db1cca22535148260b3743d0322b3799e
Luiz Felipe <lflimeira02@gmail.com>
2018-03-15 02:03:36 -0300

    Refactor webpack configuration and index example

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,14 +3,11 @@ import { join } from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/index',
+  entry: './index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'spotifyWrapper',
-  },
-  node: {
-   fs: "empty"
+    library: 'SpotifyWrapper',
   },
   devtool: 'source-map',
   module: {
